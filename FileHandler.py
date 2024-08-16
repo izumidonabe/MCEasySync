3f9e0f2ec8a53a1d8430d2f4be94d59a2b8909d8
Izumedonabe <1826624102@qq.com>
2024-08-16 17:20:06 +0800

    实现md5处理，进一步修改Server.py

---
+++ FileHandler.py
@@ -0,0 +1,21 @@
+#本文件用于实现多种文件方法
+import os
+import shutil
+import hashlib
+
+
+def get_file_md5(file_path):
+    with open(file_path, 'rb') as f:
+        md5obj = hashlib.md5()
+        md5obj.update(f.read())
+        hash = md5obj.hexdigest()
+    return hash
+
+
+def get_all_files_md5(folder):  #获取文件夹下所有文件的md5值 走相对路径
+    files_md5 = {}
+    for root, dirs, file in os.walk(folder):
+        for f in file:
+            file_path = os.path.join(root, f)
+            files_md5[file_path] = get_file_md5(file_path)
+    return files_md5
--- Server.py
+++ Server.py
@@ -5,10 +5,13 @@ class Server:
     MinecraftServers = []
 
     def __init__(self, ip, port, **kwargs):
-        self.ip = ip
-        self.port = port
-        self.username = kwargs.get("username")
-        self.password = kwargs.get("password")
+        if kwargs.get("config_file") is not None:
+            self.__dict__ = json.loads(open(kwargs.get("config_file")).read())
+        else:
+            self.ip = ip
+            self.port = port
+            self.username = kwargs.get("username")
+            self.password = kwargs.get("password")
 
 
     def add_game_server(self, server):
@@ -17,20 +20,51 @@ class Server:
     def remove_game_server(self, server):
         self.MinecraftServers.remove(server)
 
+class frpServer:
+
+        def __init__(self, **kwargs):
+            self.server_ip = kwargs.get("server_ip")
+            self.server_port = kwargs.get("server_port")
+            self.server_token = kwargs.get("server_token")
+            self.server_status = "stopped"
+
+        def save_config(self):
+            open("frp_config.json", "w").write(json.dumps(self.__dict__))
+
+        def start(self):
+            self.server_status = "running"
+
+        def stop(self):
+            self.server_status = "stopped"
+
+        def get_status(self):
+            return self.server_status
+
+        def get_config(self):
+            return self.__dict__
+
 
 class MinecraftServer:
 
     def __init__(self, **kwargs):
-        self.server_folder = kwargs.get("server_folder")
-        self.folders_to_sync = kwargs.get("folders_to_sync")
-        self.server_core = kwargs.get("server_core")
-        self.server_memory = kwargs.get("server_memory")
-        self.server_process = None
-        self.server_status = "stopped"
-        self.start_command = kwargs.get("start_command")
+        if kwargs.get("config_file") is not None:
+            self.__dict__ = json.loads(open(kwargs.get("config_file")).read())
+        else:
+            self.server_folder = kwargs.get("server_folder")
+            self.folders_to_sync = kwargs.get("folders_to_sync")
+            self.server_core = kwargs.get("server_core")
+            self.server_memory = kwargs.get("server_memory")
+            self.server_process = None
+            self.server_status = "stopped"
+            self.start_command = kwargs.get("start_command")
+
+
+
 
     def save_config(self):
         open(self.server_folder + "server_config.json", "w").write(json.dumps(self.__dict__))
+
+
 
 
 
